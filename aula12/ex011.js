7eb511ebd6efe9f5d0e39605b72f3c30b7e87dc3
Pedro Silva - Pezog <30021063+PedrBandeira@users.noreply.github.com>
2023-08-05 14:18:41 -0300

    Create ex011.js

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+// Verificando idade para votar!
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Não pode votar')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto Opcional')
+} else {
+    console.log('Voto Obrigatório')
+}
